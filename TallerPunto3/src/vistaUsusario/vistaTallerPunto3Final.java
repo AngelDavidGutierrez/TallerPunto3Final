6489fc275070de504efa81b4591ae631bef07571
miguel-cuadros <miguelcuadros1119960@correo.itm.edu.co>
2024-11-21 13:42:47 -0500

    se introduce la vista

---
+++ TallerPunto3/src/vistaUsusario/vistaTallerPunto3Final.java
@@ -0,0 +1,11 @@
+package vistaUsusario;
+
+import modelo.AlmacenCadena;
+import modelo.PuntoFabrica;
+import modelo.TiendaCentroComercial;
+import controlador.AlmacenCadenaControlador;
+import controlador.TiendaCentroComercialControlador;
+
+public class vistaTallerPunto3Final {
+
+}
